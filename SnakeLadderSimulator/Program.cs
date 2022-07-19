654ac487a111f53d5e0dfa37d60aacfedfcd4b4f
Hema <hemasahu019@gmail.com>
2022-07-19 16:24:26 +0530

    [Hema Added UC6 show position after every die roll and die roll count to win]

--- SnakeLadderSimulator/Program.cs
+++ SnakeLadderSimulator/Program.cs
@@ -1,35 +1,40 @@
 ﻿namespace SnakeLadderSimulator
 { /// <summary>
-  /// UC5 : Ensure the player gets to exact winning position 100. 
-  /// In case the player position go above 100, player stays in the same previous position till the player gets the exact
+  /// UC6 : Report the number of times the dice was played to win the game and also the position after every die role
   /// </summary>
     public class Program
     {
         //Entrypoint of console app
         static void Main(string[] args)
-        {
+        {   //Local Variables
             int position = 0;
+            int dieRollCount = 0;
 
-            //constant variable
+            //constant variables
             const int START_POSITION = 0;
             const int WINNING_POSITION = 100;
 
             Console.WriteLine("Welcome to the Snake and Ladder Simulator Program");
+            Console.WriteLine("Mode : Single Player");
             Console.WriteLine("\nThe Position of your TOKEN in the board is START : " + START_POSITION);
 
             Random random = new Random(); //Creating Random class object to access Next function
 
             while (position != WINNING_POSITION)
-            {
-                int dieRollNum = random.Next(1, 7); //using next function to generate number between 1 & 6
+            {   
+                int dieRollNum = random.Next(1, 7);
+                dieRollCount++; //incrementing dieRollCount everytime die roll to get num. of times Die rolled
                 Console.WriteLine("\nAfter Rolling die, Dice Number : " + dieRollNum);
 
-                int option = random.Next(1, 4); //Random function to generate options between 1 & 3
+                int option = random.Next(1, 4);
 
-                //Creating object of CheckOption to access CheckPlayOption method
+                //Creating object of ReturnPosition class 
                 ReturnPosition returnPositionObj = new ReturnPosition();
-                position = returnPositionObj.ReturnCurrentPosition(option, dieRollNum, position); //storing current position to run the loop
+                position = returnPositionObj.ReturnCurrentPosition(option, dieRollNum, position); //storing current position to run the  while loop
 
+                //Printing Die count once player reaches 100
+                if (position == WINNING_POSITION)
+                    Console.WriteLine("\nThe number of times Die rolled to Win the Game : " + dieRollCount);
             }
 
         }
--- SnakeLadderSimulator/ReturnPosition.cs
+++ SnakeLadderSimulator/ReturnPosition.cs
@@ -10,14 +10,14 @@ namespace SnakeLadderSimulator
     {
         //Method to match option, move token accordingly and return current position
         public int ReturnCurrentPosition(int option, int dieRollNum, int position)
-        {
-            //Selection statement to select option and change position accordingly
+
+        {   //Selection statement to select option and change position accordingly
             switch (option)
             {
                 case 1: //For Option : No Play
                     Console.WriteLine("Option : No Play");
                     position = position;
-                    Console.WriteLine("Your Current Position in board : " + position);
+                    Console.WriteLine("Your TOKEN Position in the board : " + position);
                     break;
 
                 case 2: // For Option : Ladder
@@ -27,7 +27,7 @@ namespace SnakeLadderSimulator
                     if (position > 100) // checking if player gets the position > 100
                         position -= dieRollNum; //if yes, subtracting position from dice number to get the previous position
 
-                    Console.WriteLine("Your Current Position in board : " + position);
+                    Console.WriteLine("Your TOKEN Position in the board : " + position);
 
                     break;
 
@@ -38,10 +38,11 @@ namespace SnakeLadderSimulator
                     if (position < 0) //if position becomes less than 0, player restarts from START i.e 0
                         position = 0;
 
-                    Console.WriteLine("Your Current Position in board : " + position);
+                    Console.WriteLine("Your TOKEN Position in the board : " + position);
                     break;
             }
-            return position;
+
+            return position; //returning current position
 
         }
     }
