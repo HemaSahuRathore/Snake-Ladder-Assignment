a87f2dc8dca46ad071d959c5f68c3c8f2b061c8c
Hema <hemasahu019@gmail.com>
2022-07-19 15:29:34 +0530

    [Hema Added UC1 Single Player at Start Position 0]

--- SnakeLadderSimulator/Program.cs
+++ SnakeLadderSimulator/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace SnakeLadderSimulator
-{
-    internal class Program
+{ /// <summary>
+  /// UC1 : Snake and Ladder game played with single player at start position 0 
+  /// </summary>
+    public class Program
     {
+        //Entrypoint of console app
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int START_POSITION = 0;
+
+            Console.WriteLine("Welcome to the Snake and Ladder Simulator Program");
+            Console.WriteLine("\nThe Position of your TOKEN in the board is START : " + START_POSITION);
+            Console.ReadLine();
         }
     }
 }
