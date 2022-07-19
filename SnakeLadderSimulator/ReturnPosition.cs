e37b656b67bc802fec1d7e5e5e6f5a8251ce3cda
Hema <hemasahu019@gmail.com>
2022-07-19 16:12:24 +0530

    [Hema Added UC5 Refactoring code to stay in prev. position until reached exact 100]

--- SnakeLadderSimulator/ReturnPosition.cs
+++ SnakeLadderSimulator/ReturnPosition.cs
@@ -9,27 +9,36 @@ namespace SnakeLadderSimulator
     public class ReturnPosition
     {
         //Method to match option, move token accordingly and return current position
-        public int ReturnCurrentPosition(int option, int dieRoll, int position)
+        public int ReturnCurrentPosition(int option, int dieRollNum, int position)
         {
             //Selection statement to select option and change position accordingly
             switch (option)
             {
-                case 1: //No Play
+                case 1: //For Option : No Play
                     Console.WriteLine("Option : No Play");
                     position = position;
-                    Console.WriteLine("Your Current Position : " + position);
+                    Console.WriteLine("Your Current Position in board : " + position);
                     break;
-                case 2: //Ladder
+
+                case 2: // For Option : Ladder
                     Console.WriteLine("Option : Ladder");
-                    position += dieRoll;
-                    Console.WriteLine("Your Current Position : " + position);
+                    position += dieRollNum;
+
+                    if (position > 100) // checking if player gets the position > 100
+                        position -= dieRollNum; //if yes, subtracting position from dice number to get the previous position
+
+                    Console.WriteLine("Your Current Position in board : " + position);
+
                     break;
-                default: //Snake
+
+                default:  // For Option : Snake
                     Console.WriteLine("Option : Snake");
-                    position -= dieRoll;
+                    position -= dieRollNum;
+
                     if (position < 0) //if position becomes less than 0, player restarts from START i.e 0
-                    { position = 0; }
-                    Console.WriteLine("Your Current Position : " + position);
+                        position = 0;
+
+                    Console.WriteLine("Your Current Position in board : " + position);
                     break;
             }
             return position;
